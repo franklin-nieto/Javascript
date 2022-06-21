187deed9ababdb551398951c8a4c4e59f1974e34
Franklin nieto <framanic15@gmail.com>
2022-06-20 21:44:57 -0400

    Desafio complementario

---
+++ index.js
@@ -0,0 +1,15 @@
+//Ciclo For
+for(let i = 4; i>=1; i--){
+    console.log("iteracion", i);
+}
+console.log("fin del ciclo");
+
+//Ciclo while
+
+let x = 1;
+
+while(x <= 4){
+    console.log("Hola mundo", x);
+    x++;
+}
+console.log("fin del ciclo");
